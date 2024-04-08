c7fab95705e965d6a430b28b2fc0bbe3ca257cc3
Azzaldeen98 <azzaldeen771211417@gmail.com>
2024-04-08 06:27:04 +0300

    last Commint  9-4-2024

---
+++ app/build/generated/ap_generated_sources/debug/out/com/example/esds2s/BR.java
@@ -0,0 +1,5 @@
+package com.example.esds2s;
+
+public class BR {
+  public static final int _all = 0;
+}
---
+++ app/build/generated/data_binding_trigger/debug/com/example/esds2s/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.example.esds2s;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
